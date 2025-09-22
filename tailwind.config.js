d75fda3f5506f8503dbd6016734806aa0e99c4e6
Lux VEGBA <vegbahermerite@gmail.com>
2025-09-22 21:45:23 +0200

    chore: configure TailwindCSS, PostCSS, and editor settings

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+theme: {
+  extend: {
+    colors: {
+      'background': '#111111',
+      'primary-text': '#FFFFFF',
+      'accent': '#A020F0',
+      'secondary': '#FFD700',
+    },
+  },
+},
+  plugins: [],
+}
